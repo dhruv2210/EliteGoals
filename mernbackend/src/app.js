e2648fab1d54dc76862f34a4beb5b143cfa57d3b
Dev <107358422+Devvashi09@users.noreply.github.com>
2023-03-07 18:25:43 +0530

    Payment-gateway

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,8 +1,80 @@
 const dotenv=require('dotenv');
 const express = require("express");
 const path = require("path");
-const app = express();
+// const app = express();
 
+// ________________dev_______________________
+const app = require('express')()
+// const path = require('path')
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const razorpay = new Razorpay({
+	key_id: 'rzp_test_cMV1GfmpfhYe5T',
+	key_secret: 'OCf0XPewnJgyDfNikiOlPSWJ'
+})
+
+app.get('/logo.svg', (req, res) => {
+	res.sendFile(path.join(__dirname, 'logo.svg'))
+})
+
+app.post('/verification', (req, res) => {
+	// do a validation
+	const secret = '12345678'
+
+	console.log(req.body)
+
+	const crypto = require('crypto')
+
+	const shasum = crypto.createHmac('sha256', secret)
+	shasum.update(JSON.stringify(req.body))
+	const digest = shasum.digest('hex')
+
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		console.log('request is legit')
+		// process it
+		require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+	} else {
+		// pass it
+	}
+	res.json({ status: 'ok' })
+})
+
+app.post('/razorpay', async (req, res) => {
+
+    const amount=5000
+	const payment_capture = 1
+	const currency = 'INR'
+
+	const options = {
+		amount: amount * 100,
+		currency,
+		receipt: shortid.generate(),
+		payment_capture
+	}
+
+	try {
+		const response = await razorpay.orders.create(options)
+		console.log(response)
+		res.json({
+			id: response.id,
+			currency: response.currency,
+			amount: response.amount
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
+
+
+// ---------------Ayush---------------------------------------------------
 dotenv.config({path:'./config.env'});
 //const hbs = require("hbs");
 
